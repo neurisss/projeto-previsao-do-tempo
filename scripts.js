e174ec968bc84940014a81f5851fa49544c77356
Neuris <140021259+neurisss@users.noreply.github.com>
2023-11-08 15:13:37 -0300

    ok

--- scripts.js
+++ scripts.js
@@ -4,21 +4,27 @@ const key = "99eabd051f85bac449c48ff1d47cf2ce"
 
 function colocarDadosNaTela(dados) {
 
-}
+    document.querySelector(".cidade").innerHTML = "Tempo em " +  dados.name
+    document.querySelector(".temp").innerHTML = dados.main.temp + "°C"
+    document.querySelector(".text-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+     }
 
-async function buscarCidade() {
+async function buscarCidade(cidade) {
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`).then( resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+
+    console.log(dados)
     
-    (dados)
+    colocarDadosNaTela(dados)
 
 }
 
 function cliqueiNoBotao() {
-    const input = document.querySelector(".input-cidade").value
+    const cidade = document.querySelector(".input-cidade").value
 
-    console.log(input)
 
-    buscarCidade(cidade)  
+    buscarCidade(cidade)
     
 }
