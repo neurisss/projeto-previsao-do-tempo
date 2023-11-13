39097eecd5c3e3aa7061a98c3f08714a368d7c97
Neuris <140021259+neurisss@users.noreply.github.com>
2023-11-13 15:10:13 -0300

    ok

--- scripts.js
+++ scripts.js
@@ -5,8 +5,8 @@ const key = "99eabd051f85bac449c48ff1d47cf2ce"
 function colocarDadosNaTela(dados) {
 
     document.querySelector(".cidade").innerHTML = "Tempo em " +  dados.name
-    document.querySelector(".temp").innerHTML = dados.main.temp + "°C"
-    document.querySelector(".text-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
     document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
      }
